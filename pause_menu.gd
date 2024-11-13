3e1a8558dec31355b7786eb482fba16acccbc23f
James Seton <jamesseton6@gmail.com>
2024-11-13 11:43:12 +1030

    This still isn't working

--- pause_menu.gd
+++ pause_menu.gd
@@ -5,21 +5,27 @@ extends CanvasLayer
 func _ready() -> void:
 	visible = false
 
+
 func toggle_pause() -> void:
 	if visible:
+		print("Hiding pause menu")
 		get_tree().paused = false
 		visible = false
 	else:
+		print("Showing pause menu")
 		get_tree().paused = true
 		visible = true
 
 func _on_resume_button_pressed() -> void:
 	toggle_pause()
+	print("toggle")
 	
 
 func _on_restart_button_pressed() -> void:
+	print("restart")
 	get_tree().reload_current_scene()
 	
 
 func _on_quit_button_pressed() -> void:
+	print("quit")
 	get_tree().quit()
